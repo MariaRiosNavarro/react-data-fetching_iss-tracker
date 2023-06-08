701b7a4541a2532d7ed99b52782271f120f857ab
Maria Rios Navarro <propscode254@gmail.com>
2023-06-08 19:29:43 +0200

    import SWR

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -4,14 +4,27 @@ import Map from "../Map/index";
 
 //3. import SWR (swr allready intalled- install with npm i swr)
 
-import { SWRConfig } from "swr";
+import { useSWR } from "swr";c
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 //4. add the function: const fetcher = (...args) => fetch(...args).then((res) => res.json());
-const fetcher = (URL) => fetch(URL).then((res) => res.json());
+// const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
-//1.- I change the name of the state to recognise it quickly. coords to coordState...
+const fetcher = async (URL) => {
+  const res = await fetch(URL);
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
 
 export default function ISSTracker() {
   // const [coordsState, setCoordsState] = useState({
@@ -21,7 +34,7 @@ export default function ISSTracker() {
 
   //5.- useSWR
 
-  const { data, error, isLoading } = useSWR(URL, fetcher);
+  const { data, isLoading, mutate } = useSWR(URL, fetcher);
 
   if (error) return <div>failed to load</div>;
 
@@ -51,15 +64,15 @@ export default function ISSTracker() {
   // }, []);
 
   return (
-    <SWRConfig value={{ fetcher, refreshInterval: 5000 }}>
-      <main>
-        <Map longitude={data.longitude} latitude={data.latitude} />
-        <Controls
-          longitude={data.longitude}
-          latitude={data.latitude}
-          //2.- comment old version - onRefresh={getISSCoords}
-        />
-      </main>
-    </SWRConfig>
+    // <SWRConfig value={{ fetcher, refreshInterval: 5000 }}>
+    <main>
+      <Map longitude={data.longitude} latitude={data.latitude} />
+      <Controls
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onReload={() => handleReload(mutate())}
+      />
+    </main>
+    // </SWRConfig>
   );
 }
